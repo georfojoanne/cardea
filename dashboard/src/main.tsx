1721e7f4d0d83d5465ec5f80c3c1f288ed77e000
Joanne R. Georfo <159895689+georfojoanne@users.noreply.github.com>
2026-01-05 23:24:15 +0800

    feat(ui): add login page

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // Import these
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './components/LoginPage.tsx' // Import your Login Page
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* This tells React: When path is "/", show LoginPage */}
+        <Route path="/" element={<LoginPage />} />
+        
+        {/* When path is "/dashboard", show the App (Dashboard) */}
+        <Route path="/dashboard" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
